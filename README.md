d6c584d2719298dd06f9d51f30e703c420aaf1b9
germanger <givicencio@gmail.com>
2015-04-14 14:44:58 -0300

    v0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # angular-vertical-resizer
 An angular directive for vertical resizing divs or other objects
+
+## Installation
+
+1. Download manually or using bower:
+
+        bower install angular-vertical-resizer --save
+
+2. Then add to your project:
+
+   <link rel="stylesheet" href="vertical-resizer.min.css" />
+   
+   <script src="angular.min.js"></script>
+   <script src="vertical-resizer.min.js"></script>
+   
+## Usage example
+
+    <div id="div1" style="background-color: #ffdbdb; padding: 10px; height: 80px;"><p>I'm a resizable div</p></div>
+    <vertical-resizer target-min-height="50px" target-max-height="150px" target-selector="#div1"></vertical-resizer>
+
+Check `examples.html` for more examples
+
+## Attributes
+
+ - `target-selector`: A dom selector for the object being resized, eg: `#foo`
+ - `target-max-height` (optional): Specify if the target has a max height restriction
+ - `target-min-height` (optional): Specify if the target has a min height restriction
+ - `class` (optional): If not specified, the default class `.germanger-vertical-resizer` will be used
+
+## Contributing
+
+1. Clone
+2. Run `bower install` (installs angular)
+3. Run `npm install` (installs grunt, and grunt plugins)
+4. Run `grunt` (generates minified versions)
+
---
+++ vertical-resizer.js
@@ -0,0 +1,60 @@
+angular.module('verticalResizer', []);
+
+angular.module('verticalResizer').directive('verticalResizer', ['$document', '$window', function($document, $window) {
+
+    return {
+        restrict: 'E',
+        scope: {
+            verticalResizerClass: '&'
+        },
+        template: '<div class="{{verticalResizerClass}}"></div>',
+        link: function($scope, $element, $attrs) {
+        
+            var targetElement = angular.element(document.querySelector($attrs['targetSelector']));
+
+            if (!$attrs['class']) {
+                $scope.verticalResizerClass = "germanger-vertical-resizer";
+            } else {
+                $scope.verticalResizerClass = $attrs['class'];
+            }
+
+            $element.on('mousedown', function(event) {
+                event.preventDefault();
+
+                $document.on('mousemove', mousemove);
+                $document.on('mouseup', mouseup);
+            });
+
+            function mousemove(event) {
+                var scale = event.movementY;
+                var currentHeight = parseInt(window.getComputedStyle(targetElement[0], null).getPropertyValue("height"));
+                var newHeight = currentHeight + scale;
+
+                if ($attrs['targetMinHeight']) {
+                    if (newHeight < parseInt($attrs['targetMinHeight'])) {
+                        return;
+                    }
+                }
+                
+                if ($attrs['targetMaxHeight']) {
+                    if (newHeight > parseInt($attrs['targetMaxHeight'])) {
+                        return;
+                    }
+                }
+
+                targetElement.css({
+                    height: newHeight + 'px'
+                });
+
+                // Trigger resize
+                $window.dispatchEvent(new Event('resize'));
+            };
+
+            function mouseup(event) {
+                $document.unbind('mousemove', mousemove);
+                $document.unbind('mouseup', mouseup);
+            };
+        }
+    };
+
+}]);
